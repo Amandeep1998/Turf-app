e9283181ecb701822e7f8933c3b14cc94cb8388f
Amandeep <amandeepsaini336@gmail.com>
2018-08-27 01:55:12 +0530

    Added Responsive Layout

--- routes/pages.js
+++ routes/pages.js
@@ -32,27 +32,30 @@ router.get('/searchTurfs', async(req, res) => {
   var city = req.query.city;
   var area = req.query.area;
   var preferedFormat = req.query.preferedFormat;
-
-  try {
-    if(preferedFormat == "any") {
-      var turfs = await Turf.find({city, area});
-    } else {
-      var turfs = await Turf.find({city, area, preferedFormat});
+  if(city == "Select City") {
+    res.redirect('/');
+  } else {
+    try {
+      if(preferedFormat == "any") {
+        var turfs = await Turf.find({city, area});
+      } else {
+        var turfs = await Turf.find({city, area, preferedFormat});
+      }
+      var cities = await City.find({});
+      var areas = await Area.find({});
+      var formats = await Format.find({});
+    } catch (e) {
+      console.log(e);
+      res.sendStaus(400);
     }
-    var cities = await City.find({});
-    var areas = await Area.find({});
-    var formats = await Format.find({});
-  } catch (e) {
-    console.log(e);
-    res.sendStaus(400);
+    res.render('index', {
+      turfs: turfs,
+      cities: cities,
+      areas: areas,
+      formats: formats,
+      heading: `${city} - ${area}`
+    });
   }
-  res.render('index', {
-    turfs: turfs,
-    cities: cities,
-    areas: areas,
-    formats: formats,
-    heading: `${city} - ${area}`
-  });
 });
 
 module.exports = router;
